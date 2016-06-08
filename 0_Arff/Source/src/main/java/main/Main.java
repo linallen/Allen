25d1a5dbd1d824e2802a2c8ea10c8ba7d7b77b5d
Allen Lin <linallen5460@gmail.com>
2016-06-08 23:28:29 +1000

    commit

---
+++ 0_Arff/Source/src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import arff.Arff;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("ARFF Main()");
+		Arff.main(args);
+	}
+}
