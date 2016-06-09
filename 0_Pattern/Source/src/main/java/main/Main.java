3732f1d1b99b6e933802477ccd07e145419b6a3d
Allen Lin <linallen5460@gmail.com>
2016-06-09 10:46:33 +1000

    Allen

--- 0_Pattern/Source/src/main/java/main/Main.java
+++ 0_Pattern/Source/src/main/java/main/Main.java
@@ -1,11 +1,11 @@
-package pattern;
+package main;
 
 /**
  * Classes and functions commonly used in pattern mining and analysis modules.
  * 
  * @author Allen Lin 27 Jan 2015
  */
-public class _readMe {
+public class Main {
 	public static void main(String[] args) throws Exception {
 	}
 }
